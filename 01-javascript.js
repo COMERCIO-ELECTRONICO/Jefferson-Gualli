19800fb1c96f74a3b48aab88f16514b3be9bd297
jefferson <jeffri.7751@gmail.com>
2020-05-06 12:13:18 -0500

    objetos json

---
+++ 01-javascript.js
@@ -0,0 +1,14 @@
+var json ={
+Nombre: 'jefferson',
+Edad: 23,
+Soltero: true,
+Fecha: new Date(1997-07-16),
+}
+json.nombre = 'billy'
+json.direccion= 'guamani'
+console.log(json)
+/*console.log()
+console.log()
+console.log()
+*/
+
