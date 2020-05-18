f207f583a92a9dc45262ab500b6d8dcdd79dd0e7
jefferson <jeffri.7751@gmail.com>
2020-05-18 11:45:50 -0500

    nglf-ngfor-emmet

--- proyecto-estilos/src/app/app.component.ts
+++ proyecto-estilos/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto-estilos';
+  esconderHola = false;
+  arregloNuemros = [{ nombre: 'Jefferson' }, { nombre: 'David' }];
+
+  enventoClick(){
+    this.esconderHola = true;
+  }
 }
