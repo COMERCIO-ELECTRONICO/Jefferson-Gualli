731e14f927cdc2a10de89f8637433d5f4a610c35
jefferson <jeffri.7751@gmail.com>
2020-05-20 21:17:33 -0500

    Deber-forms

--- rutas-angular/src/app/login/login.component.ts
+++ rutas-angular/src/app/login/login.component.ts
@@ -8,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-// email = '';
-// contrasena = '';
+
 nombre = '';
 apellido = '';
 celular = '';
